f4cad2bd02e0bd23ed2bad19b35c90d641f39b6a
SedricOuambo <ouambosedric@gmail.com>
2024-04-03 16:09:59 -0400

    Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        unoptimized: true
+    },
+    output: 'export'
+};
 
 export default nextConfig;
