17fff8f1a9e1c53133c0c1529db3c8c7a8df0e17
SedricOuambo <ouambosedric@gmail.com>
2024-04-18 20:21:21 -0400

    new new

--- next.config.js
+++ next.config.js
@@ -1,9 +1,44 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         unoptimized: true
+//     },
+//     output: 'export'
+// };
+// module.exports = nextConfig
+
+
+// import { withPWA } from 'next-pwa';
+// import { isProd } from 'next-pwa'; // Cette importation n'est pas nécessaire si vous avez déjà une façon de détecter si l'application est en mode production
+
+// export default withPWA({
+//     pwa: {
+//         dest: 'public',
+//         disable: !isProd // Activer le service worker uniquement en mode production
+//     },
+//     images: {
+//         unoptimized: true
+//     },
+//     output: 'export'
+// });
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
+    images: {
         unoptimized: true
     },
     output: 'export'
 };
 
-module.exports = nextConfig
+const withPWA = require('next-pwa');
+const { withPlugins } = require('next-compose-plugins');
+
+module.exports = withPlugins([
+    [withPWA, {
+        pwa: {
+            dest: 'public',
+            disable: process.env.NODE_ENV !== 'production' // Activer le service worker uniquement en mode production
+        }
+    }]
+], nextConfig);
