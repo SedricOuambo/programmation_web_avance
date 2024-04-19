8f461f68b7334cecdd958b65c6b121b9d9c4ee43
SedricOuambo <ouambosedric@gmail.com>
2024-04-18 21:31:09 -0400

    neww

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        unoptimized: true
-    },
-    output: 'export'
-};
-module.exports = nextConfig
-
-// const withPWA = require('next-pwa')({
-//     dest: 'public'
-// })
-
-// module.exports = withPWA({
-//     // next.js config
+// const nextConfig = {
 //     images: {
 //         unoptimized: true
 //     },
 //     output: 'export'
-// })
+// };
+// module.exports = nextConfig
+
+const withPWA = require('next-pwa')({
+    dest: 'public'
+})
+
+module.exports = withPWA({
+    // next.js config
+    images: {
+        unoptimized: true
+    },
+    output: 'export'
+})
