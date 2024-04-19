70867533fd6e81eb10d81054c9c3ea2f5f3ad5ba
SedricOuambo <ouambosedric@gmail.com>
2024-04-18 21:05:27 -0400

    new manifest

--- next.config.js
+++ next.config.js
@@ -1,28 +1,20 @@
-/** @type {import('next').NextConfig} */
-const isProd = require('next-pwa');
-const nextConfig = {
-    pwa: {
-        dest: 'public',
-        disable: !isProd // Activer le service worker uniquement en mode production
-    },
-    images: {
-        unoptimized: true
-    },
-    output: 'export'
-};
-module.exports = nextConfig
-
-
-// const withPWA = require('next-pwa');
-// const isProd = require('next-pwa');// Cette importation n'est pas nécessaire si vous avez déjà une façon de détecter si l'application est en mode production
-
-// export default withPWA({
-//     pwa: {
-//         dest: 'public',
-//         disable: !isProd // Activer le service worker uniquement en mode production
-//     },
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
 //     images: {
 //         unoptimized: true
 //     },
 //     output: 'export'
-// });
+// };
+// module.exports = nextConfig
+
+const withPWA = require('next-pwa')({
+    dest: 'public'
+})
+
+module.exports = withPWA({
+    // next.js config
+    images: {
+        unoptimized: true
+    },
+    output: 'export'
+})
