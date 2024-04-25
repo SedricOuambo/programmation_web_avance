605641a45f0b1bed1cd26031a26843531fb133c2
SedricOuambo <ouambosedric@gmail.com>
2024-04-25 13:47:45 -0400

    sssssssss

--- next.config.js
+++ next.config.js
@@ -1,39 +1,39 @@
-// @ts-check
-/** @type {import("next").NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    // next.js config
-    images: {
-        unoptimized: true
-    },
-    output: 'export'
-};
-
-// You may want to use a more robust revision to cache
-// files more efficiently.
-// A viable option is `git rev-parse HEAD`.
-const revision = crypto.randomUUID();
-
-module.exports = async () => {
-    const withSerwist = (await import("@serwist/next")).default({
-        cacheOnNavigation: true,
-        swSrc: "app/sw.ts",
-        swDest: "public/sw.js",
-        additionalPrecacheEntries: [{ url: "/~offline", revision }],
-    });
-    return withSerwist(nextConfig);
-};
-
-
-
-// /** @type {import('next').NextConfig} */
+// // @ts-check
+// /** @type {import("next").NextConfig} */
 // const nextConfig = {
+//     reactStrictMode: true,
+//     // next.js config
 //     images: {
 //         unoptimized: true
 //     },
 //     output: 'export'
 // };
-// module.exports = nextConfig
+
+// // You may want to use a more robust revision to cache
+// // files more efficiently.
+// // A viable option is `git rev-parse HEAD`.
+// const revision = crypto.randomUUID();
+
+// module.exports = async () => {
+//     const withSerwist = (await import("@serwist/next")).default({
+//         cacheOnNavigation: true,
+//         swSrc: "app/sw.ts",
+//         swDest: "public/sw.js",
+//         additionalPrecacheEntries: [{ url: "/~offline", revision }],
+//     });
+//     return withSerwist(nextConfig);
+// };
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        unoptimized: true
+    },
+    output: 'export'
+};
+module.exports = nextConfig
 
 
 
