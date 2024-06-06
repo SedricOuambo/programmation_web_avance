dba62963821abf6f8150374097e84f1b00c46d70
SedricOuambo <ouambosedric@gmail.com>
2024-06-06 11:33:15 -0400

    Line-numbers-codeblock

--- components/CodeBlock.jsx
+++ components/CodeBlock.jsx
@@ -1,12 +1,14 @@
 'use client'
-import { useState } from 'react';
-import 'prismjs';
+import { useState, useEffect } from 'react';
+import Prism from 'prismjs';
 import 'prismjs/components/prism-javascript'; // Chargez le langage js
 import 'prismjs/components/prism-css'; // Chargez le langage css
 import 'prismjs/components/prism-jsx'; // Chargez le langage jsx
 import 'prismjs/components/prism-xml-doc'; // Chargez le langage xml
+import 'prismjs/components/prism-python'; //Charger le langage python
+import 'prismjs/components/prism-csharp';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
-import Prism from 'prismjs';
 import styles from './CodeBlock.module.css';
 import { FaRegCopy } from "react-icons/fa6";
 
@@ -23,8 +25,12 @@ export default function Terminal(props) {
     };
 
     const highlightedCode = Prism.highlight(props.contenu, Prism.languages[props.langage], props.langage);
+    useEffect(() => {
+        window.PRISM_DEFAULT_THEME = false;
+        Prism.highlightAll();
+    }, [props.contenu]);
 
-    return <div className={styles.container + ' ' +(props.type === 'terminal' ? styles.prompt : styles.editor)}>
+    return <div className={styles.container + ' ' + (props.type === 'terminal' ? styles.prompt : styles.editor)}>
         <div className={styles.terminal}>
             {props.type === 'terminal' && 'Terminal'}
             {!props.type && props.chemin}
@@ -33,9 +39,19 @@ export default function Terminal(props) {
             </div>
         </div>
         <div className={styles.contenu}>
-            <pre>
+            {/* <pre>
                 <code dangerouslySetInnerHTML={{ __html: highlightedCode }} />
+            </pre> */}
+            <pre className="line-numbers">
+                <code className={`language-${props.langage}`}>{props.contenu}</code>
             </pre>
+
+            {/* <pre className="line-numbers">
+                <code
+                    className={`language-${props.langage}`}
+                    dangerouslySetInnerHTML={{ __html: highlightedCode }}
+                />
+            </pre> */}
         </div>
     </div>
 }
