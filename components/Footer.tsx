05ef295c6d2b454408b762b366e3582f1fac301e
SedricOuambo <ouambosedric@gmail.com>
2024-06-15 22:46:13 -0400

    installPrompt

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import styles from './Footer.module.css'
 
 export default function Footer() {
     return <footer className={styles.footer}>
-        <div>&copy; Collège La Cité | Ottawa - Canada</div>
+        <div>&copy; Collège La Cité | Ottawa, Canada</div>
         <span>
             <div>Développé par </div>
             <div>Sédric Ouambo Silatchom</div>
