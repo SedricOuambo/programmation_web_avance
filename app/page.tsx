9bbc55b1b00705207fb07bdcf6c971bb1a1f4ac5
SedricOuambo <ouambosedric@gmail.com>
2024-06-15 23:09:00 -0400

    ok ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 'use client'
 import styles from "./page.module.css";
 import Accueil from "@/components/Accueil";
-import { useModuleActif } from '@/components/Provider/ModuleActifProvider';
-import { useTitreModule } from '@/components/Provider/TitreModuleProvider';
 
 export default function Home() {
   return <>
